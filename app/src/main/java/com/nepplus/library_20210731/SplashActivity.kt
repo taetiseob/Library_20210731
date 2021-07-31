eaea1ea353d2977a6bf534535aa64fe3c0e74c7b
김태섭 <kyoer234@gmail.com>
2021-07-31 15:01:54 +0900

    Splash 화면을 추가 / 시작화면 변경

---
+++ app/src/main/java/com/nepplus/library_20210731/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.library_20210731
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

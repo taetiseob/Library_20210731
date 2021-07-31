ec982b8d9bc3670d12551f256e8025d0fa598777
김태섭 <kyoer234@gmail.com>
2021-07-31 12:22:41 +0900

    사진 크게보기 화면 추가

--- app/src/main/java/com/nepplus/library_20210731/MainActivity.kt
+++ app/src/main/java/com/nepplus/library_20210731/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.nepplus.library_20210731
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        photoImg.setOnClickListener {
+            val myIntent = Intent(this,ViewPhotoDetailActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/nepplus/library_20210731/ViewPhotoDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.nepplus.library_20210731
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ViewPhotoDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo_detail)
+
+    }
+}
